6015a530930e01f0652b9f44b72ad4d59b491047
ramya krishna <ramya.krishna@neutrinos.co>
2018-03-02 15:16:27 +0530

    click on icon -

--- app/src/app/tableComponent/table.component.ts
+++ app/src/app/tableComponent/table.component.ts
@@ -1,9 +1,9 @@
 /*DEFAULT GENERATED TEMPLATE. DO NOT CHANGE SELECTOR TEMPLATE_URL AND CLASS NAME*/
 import { Component, OnInit } from '@angular/core'
-import {MatTableDataSource} from '@angular/material';
-import {DataSource} from '@angular/cdk/collections';
-import {animate, state, style, transition, trigger} from '@angular/animations';
-import {Observable} from 'rxjs/Observable';
+import { MatTableDataSource } from '@angular/material';
+import { DataSource } from '@angular/cdk/collections';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Observable } from 'rxjs/Observable';
 
 /**
 * Model import Example :
@@ -16,33 +16,36 @@ import {Observable} from 'rxjs/Observable';
  */
 
 @Component({
-    selector: 'bh-table',
-    templateUrl: './table.template.html',
-   animations: [
+  selector: 'bh-table',
+  templateUrl: './table.template.html',
+  animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', visibility: 'hidden'})),
-      state('expanded', style({height: '*', visibility: 'visible'})),
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ]
 })
 
 export class tableComponent implements OnInit {
-    ngOnInit() {
+  test(r) {
+    console.log(r);
+  }
+  ngOnInit() {
 
-    }
-    displayedColumns = ['addIcon','position', 'name', 'weight', 'symbol'];
+  }
+  displayedColumns = ['addIcon', 'position', 'name', 'weight', 'symbol'];
 
-    displayedColumns2 = [ 'position','symbol'];
+  displayedColumns2 = ['position', 'symbol'];
   dataSource = new ExampleDataSource();
   displayObject = false;
 
-isExpansionDetailRow = (i: number, row: any) => row.hasOwnProperty('detailRow');
+  isExpansionDetailRow = (i: number, row: any) => row.hasOwnProperty('detailRow');
 
-showdisplay(){
-  console.log("inside display");
-  this.displayObject = true;
-}
+  showdisplay() {
+    console.log("inside display");
+    this.displayObject = true;
+  }
 }
 export interface Element {
   name: string;
@@ -52,20 +55,20 @@ export interface Element {
 }
 
 const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  ];
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+];
 
 export class ExampleDataSource extends MatTableDataSource<any> {
   /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<Element[]> { 
+  connect(): Observable<Element[]> {
     const rows = [];
-    ELEMENT_DATA.forEach(element => rows.push(element, {detailRow: true, element}));
+    ELEMENT_DATA.forEach(element => rows.push(element, { detailRow: true, element }));
     return Observable.of(rows);
   }
 
-  disconnect() {}
+  disconnect() { }
 }
