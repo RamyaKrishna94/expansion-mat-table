feb175bdda6fe38acb11325aed3cbf613a98bff0
ramya krishna <ramya.krishna@neutrinos.co>
2018-03-02 16:26:37 +0530

    multiple select in mat-table

--- app/src/app/tableComponent/table.component.ts
+++ app/src/app/tableComponent/table.component.ts
@@ -52,19 +52,20 @@ export interface Element {
   position: number;
   weight: number;
   symbol: string;
+  expand : boolean;
 }
 
 const ELEMENT_DATA: Element[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', expand:false },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', expand:false  },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', expand:false  },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', expand:false  },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' , expand:false },
 ];
 
 export class ExampleDataSource extends MatTableDataSource<any> {
   /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<Element[]> {
+  connect(): any {
     const rows = [];
     ELEMENT_DATA.forEach(element => rows.push(element, { detailRow: true, element }));
     return Observable.of(rows);
